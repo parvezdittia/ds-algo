fdced2266c1fda87d4278f4d33862e6335abcc8c
Parvez Dittia <parditti@publicisgroupe.net>
2020-12-23 00:22:17 +0530

    Addedobject and array test cases to node

--- ds/__test__/SinglyLinkedList.test.js
+++ ds/__test__/SinglyLinkedList.test.js
@@ -7,4 +7,20 @@ describe("Node Test", () => {
 		expect(node.val).toBe(1);
 		expect(node.next).toBeNull();
 	});
+
+	test("Create Node with objects", () => {
+		const node = new Node({ x: 1, y: "hello world" });
+		expect(node.val.x).toBe(1);
+		expect(node.val.y).toEqual("hello world");
+		expect(node.val.z).toBeUndefined();
+		expect(node.next).toBeNull();
+	});
+
+	test("Create node with arrays", () => {
+		const node = new Node(["a", "b", "c"]);
+		expect(node.val[0]).toBe("a");
+		expect(node.val[1]).toBe("b");
+		expect(node.val[2]).toBe("c");
+		expect(node.next).toBeNull();
+	});
 });
