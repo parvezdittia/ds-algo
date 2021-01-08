abd8e081f8ce4878f8a4dc3168699b89829effaa
Parvez Dittia <parditti@publicisgroupe.net>
2021-01-09 01:50:20 +0530

    Node creation and push operation

--- ds/DoublyLinkedList.js
+++ ds/DoublyLinkedList.js
@@ -1,7 +1,33 @@
 export class Node {
-	constructor(val) {
-		this.val = val;
+	constructor(value) {
+		this.value = value;
 		this.previous = null;
 		this.next = null;
+	}
+}
+
+export class DoublyLinkedList {
+	constructor() {
+		this.head = null;
+	}
+
+	push(value) {
+		const node = new Node(value);
+
+		if (this.head === null) {
+			this.head = node;
+			return 1;
+		}
+
+		let lengthOfList = 1;
+		let pointer = this.head;
+		while (pointer.next !== null) {
+			pointer = pointer.next;
+			lengthOfList++;
+		}
+		pointer.next = node;
+		node.previous = pointer;
+		node.next = null;
+		return lengthOfList + 1;
 	}
 }
