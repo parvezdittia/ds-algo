b03521f366d18f74921333d7319b3d101b760901
Parvez Dittia <parditti@publicisgroupe.net>
2021-01-09 01:50:46 +0530

    Test cases for node creation and push

--- ds/__test__/DoublyLinkedList.test.js
+++ ds/__test__/DoublyLinkedList.test.js
@@ -1,29 +1,58 @@
-import { Node } from "../DoublyLinkedList";
+import { DoublyLinkedList, Node } from "../DoublyLinkedList";
 import { describe, expect, test } from "@jest/globals";
 
 describe("Node Test", () => {
 	test("Create Node", () => {
 		const node = new Node(1);
-		expect(node.val).toBe(1);
+		expect(node.value).toBe(1);
 		expect(node.next).toBeNull();
 		expect(node.previous).toBeNull();
 	});
 
 	test("Create Node with objects", () => {
 		const node = new Node({ x: 1, y: "hello world" });
-		expect(node.val.x).toBe(1);
-		expect(node.val.y).toEqual("hello world");
-		expect(node.val.z).toBeUndefined();
+		expect(node.value.x).toBe(1);
+		expect(node.value.y).toEqual("hello world");
+		expect(node.value.z).toBeUndefined();
 		expect(node.next).toBeNull();
 		expect(node.previous).toBeNull();
 	});
 
 	test("Create node with arrays", () => {
 		const node = new Node(["a", "b", "c"]);
-		expect(node.val[0]).toBe("a");
-		expect(node.val[1]).toBe("b");
-		expect(node.val[2]).toBe("c");
+		expect(node.value[0]).toBe("a");
+		expect(node.value[1]).toBe("b");
+		expect(node.value[2]).toBe("c");
 		expect(node.next).toBeNull();
 		expect(node.previous).toBeNull();
+	});
+});
+
+describe("Doubly Linked List Test", () => {
+	test("Creation of an DLL", () => {
+		const list = new DoublyLinkedList();
+		expect(list).toMatchObject({
+			head: null,
+		});
+	});
+
+	test("Push to DLL", () => {
+		const list = new DoublyLinkedList();
+
+		expect(list.push("a")).toBe(1);
+		expect(list).toMatchObject({
+			head: {
+				value: "a",
+				previous: null,
+				next: null,
+			},
+		});
+
+		expect(list.push("b")).toBe(2);
+		expect(list.head.previous).toBeNull();
+		expect(list.head.value).toBe("a");
+		expect(list.head.next.value).toBe("b");
+		expect(list.head.next.previous).toBe(list.head);
+		expect(list.head.next.next).toBeNull();
 	});
 });
