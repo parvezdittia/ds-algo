1aae340d02c16e924a144da213ee0081f56448eb
Parvez Dittia <parditti@publicisgroupe.net>
2021-01-21 01:52:21 +0530

    BST implementation

---
+++ ds/BinarySearchTree.js
@@ -0,0 +1,38 @@
+export class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+export class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
+
+	insert(value) {
+		const newNode = new Node(value);
+
+		if (this.root === null) {
+			this.root = newNode;
+			return this;
+		}
+
+		let pointer = this.root;
+		let subTree = "";
+
+		while (subTree === "") {
+			if (value < pointer.value) {
+				if (pointer.left === null) subTree = "left";
+				else pointer = pointer.left;
+			} else {
+				if (pointer.right === null) subTree = "right";
+				else pointer = pointer.right;
+			}
+		}
+
+		pointer[subTree] = newNode;
+		return this;
+	}
+}
