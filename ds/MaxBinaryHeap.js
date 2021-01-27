796e8f6e2468dec1aca8492b05bc020a0ef96328
Parvez Dittia <parvezdittia@gmail.com>
2021-01-27 19:14:41 +0530

    Extract max implementation

--- ds/MaxBinaryHeap.js
+++ ds/MaxBinaryHeap.js
@@ -7,6 +7,10 @@ export class MaxBinaryHeap {
 		return Math.floor((index - 1) / 2);
 	}
 
+	children(index) {
+		return [2 * index + 1, 2 * index + 2];
+	}
+
 	insert(value) {
 		let positionOfNewNode = this.values.push(value) - 1;
 
@@ -23,5 +27,43 @@ export class MaxBinaryHeap {
 		}
 
 		return this;
+	}
+
+	extractMax() {
+		let positionOfRoot = 0;
+		let positionOfLastLeafNode = this.values.length - 1;
+
+		let temp = this.values[positionOfLastLeafNode];
+		this.values[positionOfLastLeafNode] = this.values[positionOfRoot];
+		this.values[positionOfRoot] = temp;
+
+		const maxValue = this.values.pop();
+
+		let pointer = 0;
+		let pointerValue = this.values[pointer];
+		let [leftChild, rightChild] = this.children(pointer);
+		let leftChildValue = this.values[leftChild];
+		let rightChildValue = this.values[rightChild];
+
+		while (pointerValue < leftChildValue || pointerValue < rightChildValue) {
+			if (leftChildValue <= rightChildValue) {
+				let temp = this.values[rightChild];
+				this.values[rightChild] = this.values[pointer];
+				this.values[pointer] = temp;
+				pointer = rightChild;
+				pointerValue = this.values[rightChild];
+			} else {
+				let temp = this.values[leftChild];
+				this.values[leftChild] = this.values[pointer];
+				this.values[pointer] = temp;
+				pointer = leftChild;
+				pointerValue = this.values[leftChild];
+			}
+			[leftChild, rightChild] = this.children(pointer);
+			leftChildValue = this.values[leftChild];
+			rightChildValue = this.values[rightChild];
+		}
+
+		return maxValue;
 	}
 }
