7900e0c959c4657e5ef0f8c095023fc411bd7220
Parvez Dittia <parvezdittia@gmail.com>
2021-01-27 19:14:56 +0530

    Extract max test cases

--- ds/__test__/MaxBinaryHeap.test.js
+++ ds/__test__/MaxBinaryHeap.test.js
@@ -12,4 +12,32 @@ describe("Max binary heap test", () => {
 		expect(heap.insert(60)).toEqual({ values: [60, 55, 30, 22, 40] });
 		expect(heap.insert(35)).toEqual({ values: [60, 55, 35, 22, 40, 30] });
 	});
+
+	test("Extract maximum value from a heap", () => {
+		let heap = new MaxBinaryHeap();
+
+		heap.insert(55).insert(22).insert(30).insert(40).insert(60).insert(35);
+		expect(heap).toEqual({ values: [60, 55, 35, 22, 40, 30] });
+
+		expect(heap.extractMax()).toBe(60);
+		expect(heap).toEqual({ values: [55, 40, 35, 22, 30] });
+
+		expect(heap.extractMax()).toBe(55);
+		expect(heap).toEqual({ values: [40, 30, 35, 22] });
+
+		expect(heap.extractMax()).toBe(40);
+		expect(heap).toEqual({ values: [35, 30, 22] });
+
+		expect(heap.extractMax()).toBe(35);
+		expect(heap).toEqual({ values: [30, 22] });
+
+		expect(heap.extractMax()).toBe(30);
+		expect(heap).toEqual({ values: [22] });
+
+		expect(heap.extractMax()).toBe(22);
+		expect(heap).toEqual({ values: [] });
+
+		expect(heap.extractMax()).toBeUndefined();
+		expect(heap).toEqual({ values: [] });
+	});
 });
