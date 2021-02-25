be136bbdace1b2347bdbaf3af31fca349fe49ef8
Parvez Dittia <parvezdittia@gmail.com>
2021-02-25 21:24:06 +0530

    Count Zeroes implementation using Binary Search

--- search/__test__/binarySearch.test.ts
+++ search/__test__/binarySearch.test.ts
@@ -33,4 +33,44 @@ describe("Binary Search Test", () => {
 		expect(binarySearch(arr, fn(7))).toBe(1);
 		expect(binarySearch(arr, fn(5))).toBe(-1);
 	});
+
+	test("Count zeroes", () => {
+		let arr = [1, 1, 1, 1, 0, 0];
+
+		const fn = (middle: number) => {
+			const middleValue = arr[middle];
+			const leftOfMiddleValue =
+				arr[middle - 1] !== undefined ? arr[middle - 1] : 1;
+			const rightOfMiddleValue =
+				arr[middle + 1] !== undefined ? arr[middle + 1] : 0;
+
+			if (middleValue === 0) {
+				if (leftOfMiddleValue === 1 && rightOfMiddleValue === 0) {
+					return 0;
+				} else {
+					return -1;
+				}
+			} else {
+				return 1;
+			}
+		};
+
+		expect(binarySearch(arr, fn)).toBe(4);
+
+		arr = [1, 0, 0, 0];
+
+		expect(binarySearch(arr, fn)).toBe(1);
+
+		arr = [0, 0, 0, 0];
+
+		expect(binarySearch(arr, fn)).toBe(0);
+
+		arr = [1, 1, 1, 1, 1];
+
+		expect(binarySearch(arr, fn)).toBe(-1);
+
+		arr = [1, 1, 1, 1, 0];
+
+		expect(binarySearch(arr, fn)).toBe(4);
+	});
 });
