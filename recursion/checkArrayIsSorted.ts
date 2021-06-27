147486fbd22d3b6380b4db90a07d9d4a6e6ecf76
Parvez Dittia <parvezdittia@gmail.com>
2021-06-27 21:56:01 +0530

    Generic array is sorted function

--- recursion/checkArrayIsSorted.ts
+++ recursion/checkArrayIsSorted.ts
@@ -1,9 +1,12 @@
-const checkArrayIsSorted = (arr: Array<T>): boolean | undefined => {
+const checkArrayIsSorted = <T>(
+	arr: Array<T>,
+	comparator: (arg0: T, arg1: T) => boolean
+): boolean | undefined => {
 	const breakDown = (index: number): boolean => {
 		if (index === 0) return true;
 
 		if (breakDown(index - 1)) {
-			return arr[index] >= arr[index - 1];
+			return comparator(arr[index], arr[index - 1]);
 		} else {
 			return false;
 		}
